dcec7561df314d59a17380b9ed2528922e35c585
Strezoski <strezoski.g@gmail.com>
2016-02-11 11:01:31 +0100

    Updated settings file

--- agora/settings.py
+++ agora/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'component',
+    'options',
+    'owner',
+    'service',
 ]
 
 MIDDLEWARE_CLASSES = [
