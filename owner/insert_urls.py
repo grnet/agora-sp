96ee2d1315e5fc438b22fff6d8d49c62458c099d
Gjorgji Strezoski <strezoski.g@gmail.com>
2016-04-04 12:16:35 +0200

    Service Owner Url refactoring

--- owner/insert_urls.py
+++ owner/insert_urls.py
@@ -3,7 +3,7 @@ from owner import views
 
 
 urlpatterns = [
-    url(r'^insert_institution/?$', views.insert_institution),
-    url(r'^insert_contact_information/?$', views.insert_contact_information),
-    url(r'^insert_service_owner/?$', views.insert_service_owner),
+    url(r'^institution/add/?$', views.insert_institution),
+    url(r'^contact_information/add/?$', views.insert_contact_information),
+    url(r'^add/?$', views.insert_service_owner),
 ]
