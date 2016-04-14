7d129ca8b6850616848eaaa27d499a90a71c02b5
Gjorgji Strezoski <strezoski.g@gmail.com>
2016-04-14 19:18:44 +0200

    v[0-9]+ to v1 conversion

--- agora/urls.py
+++ agora/urls.py
@@ -19,14 +19,14 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^api/admin/?', admin.site.urls),
     url(r'^api/docs/?', include('rest_framework_swagger.urls')),
-    url(r'^api/v[0-9]+/(portfolio|catalogue)/services/(?P<search_type>[0-9a-zA-Z\-\_]+)/service_details/(?P<version>[0-9a-zA-Z\.]+)/service_components/?', include('component.urls')),
-    url(r'^api/v[0-9]+/(portfolio|catalogue)/services/(?P<search_type>[0-9a-zA-Z\-\_]+)/service_details/(?P<version>[0-9a-zA-Z\.]+)/service_options/?', include('options.urls')),
-    url(r'^api/v[0-9]+/(portfolio|catalogue)/services/(?P<service_name_or_uuid>[0-9a-zA-Z\-\_]+)/service_owner/?', include('owner.urls')),
-    url(r'^api/v[0-9]+/(portfolio|catalogue)/services/?', include('service.urls')),
-    url(r'^api/v[0-9]+/services/?', include('service.insert_urls')),
-    url(r'^api/v[0-9]+/owner/?', include('owner.insert_urls')),
-    url(r'^api/v[0-9]+/options/?', include('options.insert_urls')),
-    url(r'^api/v[0-9]+/component/?', include('component.insert_urls'))
+    url(r'^api/v1/(portfolio|catalogue)/services/(?P<search_type>[0-9a-zA-Z\-\_]+)/service_details/(?P<version>[0-9a-zA-Z\.]+)/service_components/?', include('component.urls')),
+    url(r'^api/v1/(portfolio|catalogue)/services/(?P<search_type>[0-9a-zA-Z\-\_]+)/service_details/(?P<version>[0-9a-zA-Z\.]+)/service_options/?', include('options.urls')),
+    url(r'^api/v1/(portfolio|catalogue)/services/(?P<service_name_or_uuid>[0-9a-zA-Z\-\_]+)/service_owner/?', include('owner.urls')),
+    url(r'^api/v1/(portfolio|catalogue)/services/?', include('service.urls')),
+    url(r'^api/v1/services/?', include('service.insert_urls')),
+    url(r'^api/v1/owner/?', include('owner.insert_urls')),
+    url(r'^api/v1/options/?', include('options.insert_urls')),
+    url(r'^api/v1/component/?', include('component.insert_urls'))
 ]
 
 handler404 = "agora.views.error404"
