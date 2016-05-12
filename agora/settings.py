e119f04c704733fb770033286a8566624b44744b
Gjorgji Strezoski <strezoski.g@gmail.com>
2016-05-12 14:27:40 +0200

    Expanded allowed hosts settings

--- agora/settings.py
+++ agora/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'secret-key'
 # SECURITY WARNING don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'agora-dev.vi-seem.eu', 'agora-dev.aris.grnet.gr' ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'agora-dev.vi-seem.eu', 'agora-dev.aris.grnet.gr', 'snf-708131.vm.okeanos.grnet.gr', 'spmt.eudat.eu'  ]
 
 
 # Application definition
