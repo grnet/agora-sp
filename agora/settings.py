9c0aa81e6e572f40cb57c80ad1a5077cf6356cdb
Gjorgji Strezoski <strezoski.g@gmail.com>
2016-10-04 14:51:59 +0200

    Removed the save avatar function from the pipeline (since we are not using it)

--- agora/settings.py
+++ agora/settings.py
@@ -264,7 +264,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details',
-    'accounts.views.save_avatar'
+    # 'accounts.views.save_avatar'
 )
 
 
