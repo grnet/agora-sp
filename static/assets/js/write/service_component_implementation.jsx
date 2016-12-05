ca61d94476ae04bca0dad738b26b432720e9b6ea
stdario <stojanovski.dario@gmail.com>
2016-12-05 20:40:32 +0100

    Added go back after edit

--- static/assets/js/write/service_component_implementation.jsx
+++ static/assets/js/write/service_component_implementation.jsx
@@ -201,7 +201,7 @@ var FormWrapper = React.createClass({
 
 
 			var parts = window.location.href.split("/");
-			var host = "https://" + parts[2];
+			var host = "http://" + parts[2];
 			var url = "";
 
 			if(this.props.source != null && this.props.source != ""){
