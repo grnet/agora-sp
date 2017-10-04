93c7ee7d54dbaba62dd029bef8f575e7f01123e6
Thanasis Katsadas <kathan@grnet.gr>
2017-10-04 17:42:09 +0300

    Adapter to build correct urls
    This adapter checks the id param, and returns a url with the
    extrapolated id, if the id is an endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,2 +1,13 @@
 import BaseAdapter from 'ember-gen-apimas/adapters/application';
-export default BaseAdapter.extend({});
+
+export default BaseAdapter.extend({
+  extract_last_token(str, sep) {
+    return str.split(sep).filter(t => t).pop();
+  },
+
+  buildURL(modelName, endpoint, snapshot, requestType, query) {
+    const id = endpoint && this.extract_last_token(endpoint, "/");
+    const url = this._super(modelName, id, snapshot, requestType, query);
+    return url;
+  },
+});
