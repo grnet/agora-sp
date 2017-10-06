e9fed667f97986c78cb394067fa30494acbb6cd9
Thanasis Katsadas <kathan@grnet.gr>
2017-10-06 13:04:03 +0300

    Add service version model

---
+++ app/gen/service-version.js
@@ -0,0 +1,27 @@
+import gen from 'ember-gen/lib/gen';
+
+export default gen.CRUDGen.extend({
+  modelName: 'service-version',
+  auth: true,
+  order: 100,
+  path: 'service-versions',
+  resourceName: 'service-versions',
+  list: {
+    layout: 'table',
+    page: {
+      title: 'Service Versions'
+    },
+    menu: {
+      label: 'Service Versions'
+    },
+    row: {
+      fields: [
+        //'id',
+        'version',
+        'status',
+        'is_in_catalogue',
+        'id_service.name'
+      ],
+    }
+  }
+});
