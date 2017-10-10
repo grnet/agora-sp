7df1c8b6519ea981aa519560b1a7d8ceac138226
Thanasis Katsadas <kathan@grnet.gr>
2017-10-10 13:12:05 +0300

    Add model custom user

---
+++ app/gen/custom-user.js
@@ -0,0 +1,17 @@
+import { AgoraGen } from '../lib/common';
+
+export default AgoraGen.extend({
+  modelName: 'custom-user',
+  order: 100,
+  path: 'custom-users',
+  resourceName: 'api/v2/custom-users',
+  list: {
+    layout: 'table',
+    page: {
+      title: 'Users'
+    },
+    menu: {
+      label: 'Users'
+    },
+  }
+});
---
+++ app/models/custom-user.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  username: DS.attr(),
+  first_name: DS.attr(),
+  last_name: DS.attr(),
+  is_active: DS.attr({ type: 'boolean' }),
+  is_staff: DS.attr({ type: 'boolean' }),
+  email: DS.attr({ type: 'email' }),
+  date_joined: DS.attr({ type: 'date' })
+});
