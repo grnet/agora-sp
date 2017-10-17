943bcc6eb602d05bb6dec13cf216789acc037947
Thanasis Katsadas <kathan@grnet.gr>
2017-10-17 15:09:00 +0300

    Create a belongsTo component inside a gen

--- app/utils/common/service-item.js
+++ app/utils/common/service-item.js
@@ -3,7 +3,7 @@ import { field } from 'ember-gen';
 const DETAILS_FIELDSETS = [
   //this creates a new referenced table inside another gen
   {
-    label: 'test',
+    label: 'service_item.cards.basic_information',
     fields: [
       field(
         'name', {
@@ -17,6 +17,50 @@ const DETAILS_FIELDSETS = [
           label: 'service_item.fields.service_area'
         }
       ),
+      field(
+        'service_trl.value', {
+          type: 'text',
+          label: 'service_trl.belongs.value'
+        }
+      ),
+    ],
+    layout: {
+      flex: [ 100, 100, 100 ]
+    }
+  },
+  {
+    label: 'service_version.belongs.cards.title',
+    fields: [
+      field('versions', {
+        modelName: 'service_version',
+        displayComponent: 'gen-display-field-table',
+        valueQuery: (store, params, model, value) => {
+          if(model.get('id')) {
+            return store.query('service_version', { id_service: model.get('id') });
+          }
+        },
+        modelMeta: {
+          row: {
+            fields: [
+              field(
+                'version', {
+                  label: 'service_version.fields.version'
+                }
+              ),
+              field(
+                'id_service.name', {
+                  label: 'service_item.belongs.name'
+                }
+              ),
+            ]
+          }
+        }
+      })
+    ]
+  },
+  {
+    label: 'service_item.cards.details',
+    fields: [
       field(
         'service_type', {
           type: 'text',
@@ -38,49 +82,43 @@ const DETAILS_FIELDSETS = [
       field(
         'description_internal', {
           type: 'text',
-          label: 'Internal Description'
+          label: 'service_item.fields.description_external'
         }
       ),
       field(
         'funders_for_service', {
           type: 'text',
-          label: 'Funders for Service'
+          label: 'service_item.fields.funders'
         }
       ),
       field(
         'value_to_customer', {
           type: 'text',
-          label: 'Value to customer'
+          label: 'service_item.fields.customer_value'
         }
       ),
       field(
         'competitors', {
           type: 'text',
-          label: 'Competitors'
+          label: 'service_item.fields.competitors'
         }
       ),
       field(
         'risks', {
           type: 'text',
-          label: 'Risks'
+          label: 'service_item.fields.risks'
         }
       ),
       field(
         'request_procedures', {
           type: 'text',
-          label: 'Request Procedures'
-        }
-      ),
-      field(
-        'service_trl.value', {
-          type: 'text',
-          label: 'Service TRL'
+          label: 'service_item.fields.procedures'
         }
       ),
       field(
         'id_service_owner.full_name', {
           type: 'text',
-          label: 'Service Owner'
+          label: 'service_owner.belongs.full_name'
         }
       ),
       field(
@@ -91,30 +129,8 @@ const DETAILS_FIELDSETS = [
       ),
     ],
     layout: {
-      flex: [ 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 ]
+      flex: [ 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 ]
     }
-  },
-  {
-    label: 'Service Versions',
-    fields: [
-      field('versions', {
-        modelName: 'service_version',
-        displayComponent: 'gen-display-field-table',
-        valueQuery: (store, params, model, value) => {
-          if(model.get('id')) {
-            return store.query('service_version', { id_service: model.get('id') });
-          }
-        },
-        modelMeta: {
-          row: {
-            fields: [
-              'version',
-              'id_service.name'
-            ]
-          }
-        }
-      })
-    ]
   },
 ];
 
