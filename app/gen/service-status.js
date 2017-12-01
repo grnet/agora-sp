815463a512dfa0c2813b860cb24431ef17b43a11
Olga Brani <olgabrani@grnet.gr>
2017-12-01 12:51:59 +0200

    Update service status gen

--- app/gen/service-status.js
+++ app/gen/service-status.js
@@ -1,17 +1,42 @@
 import { AgoraGen } from '../lib/common';
+import { field } from 'ember-gen';
+
+const COMMON_FIELDSETS = [{
+  label: 'service_status.cards.basic_information',
+  text: 'service_status.cards.basic_hint',
+  layout: {
+    flex: [50, 50]
+  },
+  fields: [
+    field(
+      'value', {
+        'label': 'service_status.fields.value'
+      }
+    ),
+    field(
+      'order', {
+        'label': 'service_status.fields.order',
+        'hint': 'service_status.hints.order'
+      }
+    )
+  ]
+}]
 
 export default AgoraGen.extend({
   modelName: 'service-status',
-  order: 100,
+  order: 300,
   path: 'service-status',
   resourceName: 'api/v2/service-status',
+  common: {
+    fieldsets: COMMON_FIELDSETS,
+  },
   list: {
     layout: 'table',
     page: {
-      title: 'Service Status'
+      title: 'service_status.menu'
     },
     menu: {
-      label: 'Service Status',
+      label: 'service_status.menu',
       group: {
         name: 'settings',
         label: 'Settings',
@@ -19,11 +44,17 @@ export default AgoraGen.extend({
       }
     },
     row: {
+      actions: ['gen:details', 'gen:edit', 'remove'],
       fields: [
-        //'id',
         'value',
         'order',
       ],
-    }
+    },
+    sort: {
+      serverSide: false,
+      active: true,
+      fields: ['order', 'value']
+    },
+
   }
 });
--- app/locales/en/service_status.js
+++ app/locales/en/service_status.js
@@ -2,9 +2,18 @@ const service_status = {
   'belongs': {
     'value': 'Service Status'
   },
+  'cards': {
+    'basic_information': 'Basic information',
+    'basic_hint': 'Service status will be used in Service Versions'
+  },
   'fields': {
     'value': 'Value',
-  }
+    'order': 'Order'
+  },
+  'hints': {
+    'order': 'Order is used only internally in admin interface',
+  },
+  'menu': 'Service Version Status',
 };
 
 export { service_status };
