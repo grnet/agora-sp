6d2f149a1d6c55e0097df12ea8bc2a4ce5b18189
Olga Brani <olgabrani@grnet.gr>
2017-12-14 15:12:53 +0200

    Use logged in user data in every session

--- app/authenticators/agora.js
+++ app/authenticators/agora.js
@@ -3,22 +3,7 @@ import ApimasAuthenticator from 'ember-gen-apimas/authenticators/apimas';
 
 export default ApimasAuthenticator.extend({
   processProfileData(data, profile) {
-   /* let user = profile.user;
-    user.user_id = user.id.toString();
-    delete user.id;
-    delete profile.user;
-
-    merge(data, user);
-    merge(data, profile);
-
-    if(data.id) {
-      data.id = data.id.toString();
-    }
-
-    if (data.hasOwnProperty('is_verified') && data.is_verified === false) {
-      data.pending_role = data.role;
-      delete data.role;
-    }*/
+    Ember.merge(data, profile);
     return data;
   }
 });
