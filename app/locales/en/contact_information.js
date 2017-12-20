0d93926d671f2a11e469929bcb244d97d7ac6f84
Olga Brani <olgabrani@grnet.gr>
2017-12-20 17:13:54 +0200

    Update contact-information gen/model

--- app/locales/en/contact_information.js
+++ app/locales/en/contact_information.js
@@ -1,16 +1,31 @@
 const contact_information = {
   'cards': {
+    'basic_information': 'Basic information',
+    'basic_hint': 'Contact information will be used in ?'
   },
   'fields': {
     'first_name': 'First Name',
     'last_name': 'Last Name',
-    'full_name': 'Full Name'
+    'full_name': 'Full Name',
+    'email': 'Email',
+    'phone': 'Phone',
+    'url': 'URL',
   },
+  'hints': {
+    'first_name': '',
+    'last_name': '',
+    'full_name': '',
+    'email': '',
+    'phone': '',
+    'url': 'URL?',
+  },
+
   'belongs': {
     'full_name': 'Full Name',
     'internal': 'Internal contact information',
     'external': 'External contact information',
-  }
+  },
+  'menu': 'Contact Information'
 };
 
 export { contact_information };
--- app/models/contact-information.js
+++ app/models/contact-information.js
@@ -2,11 +2,27 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  first_name: DS.attr(),
-  last_name: DS.attr(),
-  url: DS.attr(),
-  phone: DS.attr(),
-  email: DS.attr(),
+  first_name: DS.attr({
+    label: 'contact_information.fields.first_name',
+    hint: 'contact_information.hint.first_name'
+  }),
+  last_name: DS.attr({
+    label: 'contact_information.fields.last_name',
+    hint: 'contact_information.hints.last_name'
+  }),
+  url: DS.attr({
+    label: 'contact_information.fields.url',
+    hint: 'contact_information.hints.url'
+
+  }),
+  phone: DS.attr({
+    label: 'contact_information.fields.phone',
+    hint: 'contact_information.hints.phone',
+  }),
+  email: DS.attr({
+    label: 'contact_information.fields.email',
+    hint: 'contact_information.hints.email',
+  }),
   full_name: Ember.computed('first_name', 'last_name', function() {
     const first_name = this.get('first_name');
     const last_name = this.get('last_name');
