69bc00d935ac30021c47a5ba7f6fa2eedc78bf39
Olga Brani <olgabrani78@gmail.com>
2018-02-21 18:33:31 +0200

    tests: Add observer, admin and superadmin fixtures

--- agora/testing.py
+++ agora/testing.py
@@ -16,14 +16,49 @@ class ApimasClient(Client):
 
 
 @pytest.fixture('function')
+def superadmin(django_user_model):
+    user, created = \
+        django_user_model.objects.get_or_create(username='superadmin',
+                                                role='superadmin',
+                                                email='superadmin@test.org',
+                                                is_superuser=True)
+    user.set_password('12345')
+    user.save()
+
+    credentials = {"username": 'superadmin', "password": '12345'}
+    client = ApimasClient()
+    resp = client.post('/api/v2/auth/login/', credentials)
+    token = resp.json().get('auth_token')
+    client.set_token(token)
+    return client
+
+
+@pytest.fixture('function')
 def admin(django_user_model):
     user, created = \
         django_user_model.objects.get_or_create(username='admin',
-                                                is_superuser=True)
-    user.set_password('admin')
+                                                email='admin@test.org',
+                                                role='admin')
+    user.set_password('12345')
     user.save()
 
-    credentials = {"username": 'admin', "password": 'admin'}
+    credentials = {"username": 'admin', "password": '12345'}
+    client = ApimasClient()
+    resp = client.post('/api/v2/auth/login/', credentials)
+    token = resp.json().get('auth_token')
+    client.set_token(token)
+    return client
+
+@pytest.fixture('function')
+def observer(django_user_model):
+    user, created = \
+        django_user_model.objects.get_or_create(username='observer',
+                                                email='observer@test.org',
+                                                role='observer')
+    user.set_password('12345')
+    user.save()
+
+    credentials = {"username": 'observer', "password": '12345'}
     client = ApimasClient()
     resp = client.post('/api/v2/auth/login/', credentials)
     token = resp.json().get('auth_token')
