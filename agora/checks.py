b4ba6c724dda239158bbd7d7f71c3c2b605b0698
Olga Brani <olgabrani@grnet.gr>
2018-05-03 15:17:43 +0300

    Refactor checks.py in order to use Classes

--- agora/checks.py
+++ agora/checks.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 
+class ServiceAdminship(object):
 
-def has_serviceadminship(context):
-    auth_user = context.extract('auth/user')
-    return Q(admin=auth_user)
+    @staticmethod
+    def has_serviceadminship(context):
+        auth_user = context.extract('auth/user')
+        return Q(admin=auth_user)
