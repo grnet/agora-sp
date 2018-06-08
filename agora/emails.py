adefddaf17a295d40c3f2d8179fc68d97ad8245d
Olga Brani <olgabrani@grnet.gr>
2018-06-08 12:13:46 +0300

    Minor refactoring in shibboleth emails

--- agora/emails.py
+++ agora/emails.py
@@ -4,14 +4,14 @@ from django.template.loader import render_to_string
 
 
 def send_email_shib_user_created(user, http_host):
-    template_subject = 'emails/shib_user_created_subject.txt'
-    template_body = 'emails/shib_user_created_body.txt'
-    template_context = {
+    tpl_subject = 'emails/shib_user_created_subject.txt'
+    tpl_body = 'emails/shib_user_created_body.txt'
+    tpl_context = {
         'user': user,
         'http_host': http_host
     }
-    subject = render_to_string(template_subject, template_context).replace('\n', ' ')
-    body = render_to_string(template_body, template_context)
+    subject = render_to_string(tpl_subject, tpl_context).replace('\n', ' ')
+    body = render_to_string(tpl_body, tpl_context)
     sender = settings.DEFAULT_FROM_EMAIL
     recipient_list = settings.USER_CREATION_EMAIL_LIST
     send_mail(
