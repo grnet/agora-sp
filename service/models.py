c23ae601f3d07a0ee8f0bc292b43bc67c4bc0bee
Olga Brani <olgabrani@grnet.gr>
2018-06-18 12:11:32 +0300

    Revert "Expose user_customers to service item"
    This reverts commit f243b7044f61a32b0fea31835e43710b44e5f4ea.

--- service/models.py
+++ service/models.py
@@ -1042,7 +1042,7 @@ class UserCustomer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.ForeignKey(UserRole)
     role = RichTextUploadingField(default=None, blank=True, null=True)
-    service_id = models.ForeignKey(Service, related_name='user_customers')
+    service_id = models.ForeignKey(Service)
 
     def __unicode__(self):
         return str(self.name) + " as " + str(self.role) + " for " + str(self.service_id)
