fe83aa23062e1793daaacae41f692cf5477b4102
Olga Brani <olgabrani@grnet.gr>
2019-05-10 12:28:32 +0300

    Remove service version *_has fields
    These fields are not exposed in API anymore.

--- app/models/service-version.js
+++ app/models/service-version.js
@@ -2,23 +2,14 @@ import DS from 'ember-data';
 import gen from 'ember-gen/lib/attrs';
 
 let model = DS.Model.extend({
-  privacy_policy_has: DS.attr({ type: 'boolean' }),
   privacy_policy_url: DS.attr(),
-  monitoring_has: DS.attr({ type: 'boolean' }),
   monitoring_url: DS.attr(),
-  user_documentation_has: DS.attr({ type: 'boolean' }),
   user_manual: DS.attr(),
-  decommissioning_procedure_has: DS.attr({ type: 'boolean' }),
   decommissioning_procedure_url: DS.attr(),
-  accounting_has: DS.attr({ type: 'boolean' }),
   accounting_url: DS.attr(),
-  operations_documentation_has: DS.attr({ type: 'boolean' }),
   admin_manual: DS.attr(),
-  business_continuity_plan_has: DS.attr({ type: 'boolean' }),
   business_continuity_plan_url: DS.attr(),
-  disaster_recovery_plan_has: DS.attr({ type: 'boolean' }),
   disaster_recovery_plan_url: DS.attr(),
-  terms_of_use_has: DS.attr({ type: 'boolean' }),
   terms_of_use_url: DS.attr(),
   features_current: DS.attr(),
   features_future: DS.attr(),
