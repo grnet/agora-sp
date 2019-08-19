985e04f5938fcaa4796d90a5ccecd98242e47744
Olga  Brani <olgabrani78@gmail.com>
2019-08-19 15:49:52 +0300

    ui: Update tests

--- ui/cypress/integration/users/sidebar-menu.spec.js
+++ ui/cypress/integration/users/sidebar-menu.spec.js
@@ -1,28 +1,28 @@
 describe('sidebar menu', function() {
 
   it('observer views sidebar menu', () => {
-    let location_contains = [ 'services', 'service-versions', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers' ]
+    let location_contains = [ 'my-profile',  'services', 'service-versions', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers' ]
 
     cy.login('observer', '12345')
     cy.check_menu(location_contains)
   })
 
   it('service_admin views sidebar menu', () => {
-    let location_contains = ['services', 'service-versions', 'my-services', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'service-admins', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers' ]
+    let location_contains = ['my-profile', 'services', 'my-services', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'service-admins', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers' ]
 
     cy.login('service_admin_1', '12345')
     cy.check_menu(location_contains)
   })
 
   it('admin views sidebar menu', () => {
-    let location_contains = ['services', 'service-versions', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'service-admins', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers', 'custom-users' ]
+    let location_contains = ['my-profile', 'services', 'service-versions', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'service-admins', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers', 'custom-users' ]
 
     cy.login('admin', '12345')
     cy.check_menu(location_contains)
   })
 
   it('superadmin views sidebar menu', () => {
-    let location_contains = [ 'services', 'service-versions', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'service-admins', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers', 'custom-users' ]
+    let location_contains = ['my-profile', 'services', 'service-versions', 'components', 'component-implementations', 'component-implementation-details', 'component-implementation-detail-links', 'access-policies', 'providers', 'service-admins', 'federation-members', 'service-categories', 'service-trls', 'user-roles', 'service-status', 'user-customers', 'custom-users' ]
 
     cy.login('superadmin', '12345')
     cy.check_menu(location_contains)
