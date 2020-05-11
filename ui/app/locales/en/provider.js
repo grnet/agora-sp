1fc201fb9deea73e14d546ff0edad7b66d553dc5
Olga Brani <olgabrani@grnet.gr>
2020-05-11 09:17:00 +0000

    ui: Improve hint for agora-chip PD.CLI.3 Tag field

--- ui/app/locales/en/provider.js
+++ ui/app/locales/en/provider.js
@@ -77,7 +77,7 @@ const provider = {
     'pd_bai_4_website': 'Website with information about the Provider.',
     'pd_cli_1_scientific_domain': 'The branch of science, scientific discipline that is related to the service/resource.',
     'pd_cli_2_scientific_subdomain': 'The subbranch of science, scientific subdicipline that is related to the service/resource.',
-    'pd_cli_3_tags': 'Keywords associated to the service/resource to simplify search by relevant keywords.',
+    'pd_cli_3_tags': 'Keywords associated to the service/resource to simplify search by relevant keywords. <br/>Hit <strong>ENTER</strong> to register your tag.',
     'pd_loi_1_street_name_and_number': 'Street and number of the Provider\'s physical location.<br />example: Christou Lada Str.',
     'pd_loi_2_postal_code': 'Postal code of the Provider\'s physical location.<br />example: 10561',
     'pd_loi_3_city': 'City that the Provider is located in.<br />example: Athens',
