d65740dbf818c0f8ecec3a2f57962a7a9c0f5616
Olga Brani <olgabrani@grnet.gr>
2020-06-09 14:20:07 +0000

    ui: Remove unused code

--- ui/app/locales/en/funding_body.js
+++ ui/app/locales/en/funding_body.js
@@ -1,21 +1,3 @@
-const funding_body = {
-    'fields': {
-      'name': 'Area of Activity name',
-    },
-    'hints': {
-      'name': '',
-    },
-    'belongs': {
-      'name': 'Area of Activity',
-      'hint': ''
-    },
-    'table': {
-      'name': 'Area of Activity',
-    },
-    'placeholder':{
-      'search': 'Search by Area of Activity',
-    },
-    'menu': 'Funding Body'
+export const funding_body = {
+  'menu': 'Funding Body'
 };
-
-export { funding_body }
--- ui/app/locales/en/funding_program.js
+++ ui/app/locales/en/funding_program.js
@@ -1,19 +1,3 @@
 export const funding_program = {
-    //'fields': {
-      //'name': 'Area of Activity name',
-    //},
-    //'hints': {
-      //'name': '',
-    //},
-    //'belongs': {
-      //'name': 'Area of Activity',
-      //'hint': ''
-    //},
-    //'table': {
-      //'name': 'Area of Activity',
-    //},
-    //'placeholder':{
-      //'search': 'Search by Area of Activity',
-    //},
-    'menu': 'Funding Program'
-  };
+  'menu': 'Funding Program'
+};
