81b2b5f9963fdc1fb0573ee4378be6ddbe6e3ae4
Olga Brani <olgabrani@grnet.gr>
2020-07-08 09:34:53 +0000

    ui: Cleanup locales file

--- ui/app/locales/en/translations.js
+++ ui/app/locales/en/translations.js
@@ -1,85 +1,85 @@
-import { custom_user } from './custom_user';
-import { user_role } from './user_role';
-import { user_customer } from './user_customer';
-import { provider } from './provider';
-import { common } from './common';
-import { login } from './login';
-import { profile } from './profile';
-import { policy } from './policy';
-import { resource } from './resource';
-import { target_user } from './target_user';
-import { contact_information } from './contact_information';
-import { resource_admin } from './resource_admin';
-import { network } from './network';
-import { structure } from './structure';
+import { access_mode } from './access_mode';
+import { access_type } from './access_type';
+import { activity } from './activity';
 import { affiliation } from './affiliation';
+import { category } from './category';
+import { challenge } from './challenge';
+import { common } from './common';
+import { contact_information } from './contact_information';
+import { custom_user } from './custom_user';
+import { domain } from './domain';
 import { esfridomain } from './esfridomain';
 import { esfritype } from './esfritype';
-import { activity } from './activity';
-import { challenge } from './challenge';
-import { domain } from './domain';
-import { subdomain } from './subdomain';
-import { supercategory } from './supercategory';
-import { category } from './category';
-import { subcategory } from './subcategory';
-import { order_type } from './order_type';
-import { funding_program } from './funding_program';
 import { funding_body } from './funding_body';
-import { access_type } from './access_type';
-import { access_mode } from './access_mode';
-import { trl } from './trl';
-import { resource_lcs } from './resource_lcs';
+import { funding_program } from './funding_program';
 import { legalstatus } from './legalstatus';
+import { login } from './login';
 import { merildomain} from './merildomain';
 import { merilsubdomain} from './merilsubdomain';
+import { network } from './network';
+import { order_type } from './order_type';
+import { policy } from './policy';
+import { profile } from './profile';
+import { provider } from './provider';
+import { resource } from './resource';
+import { resource_admin } from './resource_admin';
+import { resource_lcs } from './resource_lcs';
+import { structure } from './structure';
+import { subcategory } from './subcategory';
+import { subdomain } from './subdomain';
+import { supercategory } from './supercategory';
+import { target_user } from './target_user';
+import { trl } from './trl';
+import { user_customer } from './user_customer';
+import { user_role } from './user_role';
 
 
 export default {
-  'value.not.set': '',
-  'file': common.file,
-  'message': common.message,
-  'custom_user': custom_user,
-  'user_role': user_role,
-  'user_customer': user_customer,
-  'provider': provider,
-  'form': common.form,
-  'login': login,
-  'group_menu': common.group_menu,
-  'confirm': common.confirm,
-  'list': common.list,
-  'select': common.select,
-  'row': common.row,
-  'common': common,
-  'profile': profile,
-  'policy': policy,
-  resource,
-  target_user,
-  contact_information,
   'Service Admin': 'Resource Admin',
-  resource_admin,
-  'legalstatus': legalstatus,
-  'affiliation': affiliation,
-  'network': network,
-  'structure': structure,
-  'esfridomain': esfridomain,
-  'esfritype': esfritype,
-  'activity': activity,
-  'challenge': challenge,
-  'domain': domain,
-  'subdomain': subdomain,
-  'supercategory': supercategory,
-  'category': category,
-  'subcategory': subcategory,
-  'merildomain': merildomain,
-  'merilsubdomain': merilsubdomain,
-  'username': login.default.label,
+  'confirm': common.confirm,
+  'file': common.file,
+  'form': common.form,
+  'group_menu': common.group_menu,
+  'list': common.list,
+  'message': common.message,
+  'row': common.row,
   'search.input.placeholder': 'Search',
-  order_type,
-  funding_program,
-  funding_body,
-  access_type,
+  'select': common.select,
+  'username': login.default.label,
+  'value.not.set': '',
   access_mode,
-  trl,
+  access_type,
+  activity,
+  affiliation,
+  category,
+  challenge,
+  common,
+  contact_information,
+  custom_user,
+  domain,
+  esfridomain,
+  esfritype,
+  funding_body,
+  funding_program,
+  legalstatus,
+  login,
+  merildomain,
+  merilsubdomain,
+  network,
+  order_type,
+  policy,
+  profile,
+  provider,
+  resource,
+  resource_admin,
   resource_lcs,
+  structure,
+  subcategory,
+  subdomain,
+  supercategory,
+  target_user,
+  trl,
+  user_customer,
+  user_role,
 };
 
