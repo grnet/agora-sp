f3d518db5c19bc18f7dd4b6633ccda6984918fa0
Olga Brani <olgabrani@grnet.gr>
2020-07-08 09:31:31 +0000

    ui: Remove unused files

--- ui/app/locales/en/translations.js
+++ ui/app/locales/en/translations.js
@@ -1,19 +1,7 @@
-import { service_item } from './service_item';
-import { service_version } from './service_version';
-import { service_admin } from './service_admin';
-import { service_status } from './service_status';
-import { service_category } from './service_category';
-import { component } from './component';
-import { component_implementation } from './component_implementation';
-import { component_implementation_detail } from './component_implementation_detail';
-import { cidl } from './component_implementation_detail_link';
 import { custom_user } from './custom_user';
 import { user_role } from './user_role';
 import { user_customer } from './user_customer';
-import { institution } from './institution';
 import { provider } from './provider';
-import { access_policy } from './access_policy';
-import { federation_member } from './federation_member';
 import { common } from './common';
 import { login } from './login';
 import { profile } from './profile';
@@ -48,29 +36,16 @@ import { merilsubdomain} from './merilsubdomain';
 
 export default {
   'value.not.set': '',
-  'service_item': service_item,
-  'service_version': service_version,
-  'service_admin': service_admin,
-  'service_status': service_status,
-  'service_category': service_category,
-  'component': component,
-  'component_implementation': component_implementation,
-  'component_implementation_detail': component_implementation_detail,
-  'cidl': cidl,
   'file': common.file,
   'message': common.message,
   'custom_user': custom_user,
   'user_role': user_role,
   'user_customer': user_customer,
-  'institution': institution,
   'provider': provider,
-  'federation_member': federation_member,
-  'access_policy': access_policy,
   'form': common.form,
   'login': login,
   'group_menu': common.group_menu,
   'confirm': common.confirm,
-  'Service_type should be unique': cidl.errors.service_type_unique,
   'list': common.list,
   'select': common.select,
   'row': common.row,
