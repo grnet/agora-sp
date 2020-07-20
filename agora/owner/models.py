69e42502d8be5e9a04a66cd1d704135dcff3e6c3
Olga Brani <olgabrani@grnet.gr>
2020-07-20 12:57:35 +0000

    Remove unused models frow owner app

--- agora/owner/models.py
+++ agora/owner/models.py
@@ -3,17 +3,6 @@ import uuid
 from django.db import models
 from accounts.models import User as CustomUser, Organisation
 
-class Institution(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, default=None, blank=True, unique=True)
-    address = models.CharField(max_length=255, default=None, blank=True, null=True)
-    country = models.CharField(max_length=255, default=None, blank=True, null=True)
-    department = models.CharField(max_length=255, default=None, blank=True, null=True)
-
-    def __unicode__(self):
-        return str(self.name)
-
-
 class ServiceOwner(models.Model):
     """
     Unused model
@@ -34,24 +23,3 @@ class ContactInformation(models.Model):
     phone = models.CharField(max_length=20, default=None, blank=True, null=True)
     position = models.CharField(max_length=50, default=None, blank=True, null=True)
     organisation = models.ForeignKey(Organisation,   related_name="contacts")
-
-class Internal(models.Model):
-    """
-    Unused model
-    """
-    id_contact_info = models.ForeignKey(ContactInformation)
-
-
-class External(models.Model):
-    """
-    Unused model
-    """
-    id_contact_info = models.ForeignKey(ContactInformation)
-
-
-class AditionalUsernames(models.Model):
-    """
-    Unused model
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=255, default=None, blank=True)
