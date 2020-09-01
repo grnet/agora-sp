170e8119442cb16428201b33499ebb3997d7165e
Kostas Evangelou <kevangel@admin.grnet.gr>
2020-09-01 11:35:34 +0300

    Fix tests

--- tests/selenium_tests/agora/validations/save_responses.py
+++ tests/selenium_tests/agora/validations/save_responses.py
@@ -43,6 +43,39 @@ def save_success(save_button):
     #     print("[Saving form status] {0:>30} \t\t{1}".format("Form Saved", "Success"))
     #     return True
 
+def save_success_double(save_button):
+    """
+    Save a valid form.
+
+    It tries to save the form and checks the response from the page.
+    @return: True if the form returns an success message otherwise False.
+    """
+    # Wait at most 10 seconds.
+    wait = WebDriverWait(save_button, 50)
+    # wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.visibility_of_any_elements_located((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.presence_of_element_located((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="save"]'))).click()
+    # save_button.find_element_by_xpath('//button[text()="save"]').click()
+
+    wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.visibility_of_any_elements_located((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.presence_of_element_located((By.XPATH, '//button[text()="save"]')))
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="save"]'))).click()
+
+    # wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'toast-level-success')))
+    # form_response_message = save_button.find_element_by_class_name("toast-level-success").text.split("\n")[0]
+
+    # print(save_button.current_url)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a//md-icon[@md-font-icon='edit']")))
+    # form_response_message = save_button.find_element_by_xpath("//a//md-icon[@md-font-icon='edit']").text
+
+    # assert "edit" in form_response_message
+    # if form_response_message == 'edit':
+    #     print("[Saving form status] {0:>30} \t\t{1}".format("Form Saved", "Success"))
+    #     return True
+
 
 def save_invalid(save_button):
     """
