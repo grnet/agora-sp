30cddf6569bb97cb22f82e8459d4a6ec6d4d6c09
Olga Brani <olgabrani@grnet.gr>
2020-12-18 14:04:28 +0000

    WIP: resource_post_eosc api call

--- agora/agora/settings.py
+++ agora/agora/settings.py
@@ -359,6 +359,9 @@ SETTINGS_DIR = os.environ.get('AGORA_SETTINGS_DIR', '/etc/agora')
 SETTINGS_FILE = 'settings.conf'
 SETTINGS_PATH = os.path.join(SETTINGS_DIR, SETTINGS_FILE)
 
+EOSC_API_URL = 'https://beta.providers.eosc-portal.eu/api/'
+EOSC_TOKEN = '<my-token>'
+
 if not os.path.isfile(SETTINGS_PATH):
     m = "Cannot find settings file {0!r}. Consider using AGORA_SETTINGS_DIR "
     m += "environment variable to set a custom path for settings.conf file."
