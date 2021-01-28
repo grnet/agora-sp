41376f93d3d428d55256589e40b72f96f1df68fe
Olga Brani <olgabrani@grnet.gr>
2021-01-28 12:02:20 +0000

    Set default value for published resources/providers now

--- agora/set_created_at_default.py
+++ agora/set_created_at_default.py
@@ -1,12 +1,16 @@
 import pytz
 from datetime import datetime
+from django.utils import timezone
 from service.models import Resource
 from accounts.models import Organisation
 
 resources = Resource.objects.all()
 organisations = Organisation.objects.all()
+published_resources = Resource.objects.filter(state='published')
+published_organisations = Resource.objects.filter(state='published')
 
 CREATED_AT = datetime(2020, 12, 31, 0, 0, tzinfo=pytz.UTC)
+NOW = datetime.now(timezone.utc)
 
 for r in resources:
     r.created_at = CREATED_AT
@@ -14,4 +18,12 @@ for r in resources:
 
 for o in organisations:
     o.created_at = CREATED_AT
+    o.save()
+
+for p in published_resources:
+    p.published_at = NOW
+    p.save()
+
+for o in published_organisations:
+    o.published_at = NOW
     o.save()
