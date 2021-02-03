07b0a13d6d1e1260d44a87916382470c1dd7bade
Olga Brani <olgabrani@grnet.gr>
2021-02-03 19:30:20 +0000

    WIP: POST request to eosc portal

--- agora/agora/actions.py
+++ agora/agora/actions.py
@@ -11,17 +11,29 @@ logger = logging.getLogger(__name__)
 
 
 def resource_post_eosc(backend_input, instance, context):
-    username = context['auth/user'].username
+    session = requests.Session()
     url = EOSC_API_URL+'resource'
     id  = str(instance.id)
+    username = context['auth/user'].username
     headers = {
         'Authorization': EOSC_TOKEN,
         'Accept': 'application/json',
         'Content-Type': 'application/json',
     }
-    # TODO: Perform call to EOSC PORTAL API
+    req = requests.Request('POST',url ,headers=headers,json={"name": "Foo", "resourceOrganisation": "grnet"})
+    prepared = req.prepare()
     logger.info('EOSC PORTAL API call to POST resource \
         with id %s to %s has been made by %s at %s \
         ' %(id, url, username, datetime.now()))
+    res = session.send(prepared)
 
+    logger.info('Response status code: %s' %(res.status_code))
+    logger.info('Response json: %s' %(res.json()))
 
+def pretty_print_POST(req):
+    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
