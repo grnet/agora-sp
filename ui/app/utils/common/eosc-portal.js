c895452146f3dabd3e48b656e3e73e5a08f833af
Olga Brani <olgabrani@grnet.gr>
2021-02-12 10:05:15 +0000

    ui: Use DRY principle in eosc-portal action functions

--- ui/app/utils/common/eosc-portal.js
+++ ui/app/utils/common/eosc-portal.js
@@ -9,6 +9,81 @@ const {
 
 const DISABLED = ENV.APP.eosc_portal.disabled;
 
+function prompt(self, method) {
+  const erp_bai_1_name = get(self, 'model.erp_bai_1_name');
+  const erp_bai_2_service_organisation = get(self, 'model.erp_bai_2_service_organisation.id');
+  const erp_bai_4_webpage = get(self, 'model.erp_bai_4_webpage');
+  const erp_mri_1_description = get(self, 'model.erp_mri_1_description');
+  const erp_mri_2_tagline = get(self, 'model.erp_mri_2_tagline');
+  const erp_mri_3_logo = get(self, 'model.erp_mri_3_logo');
+  const erp_cli_1_scientific_domain = get(self, 'model.erp_cli_1_scientific_domain');
+  const erp_cli_2_scientific_subdomain = get(self, 'model.erp_cli_2_scientific_subdomain');
+  const erp_cli_3_category = get(self, 'model.erp_cli_3_category');
+  const erp_cli_4_subcategory = get(self, 'model.erp_cli_4_subcategory');
+  const erp_cli_5_target_users = get(self, 'model.erp_cli_5_target_users').content.length > 0;
+  const erp_gla_1_geographical_availability = get(self, 'model.erp_gla_1_geographical_availability');
+  const erp_gla_2_language = get(self, 'model.erp_gla_2_language');
+  const main_contact = get(self, 'model.main_contact.id');
+  const public_contact = get(self, 'model.public_contact.id');
+  const erp_coi_13_helpdesk_email = get(self, 'model.erp_coi_13_helpdesk_email');
+  const erp_coi_14_security_contact_email = get(self, 'model.erp_coi_14_security_contact_email');
+  const erp_mti_1_technology_readiness_level = get(self, 'model.erp_mti_1_technology_readiness_level.id');
+  const erp_mgi_2_user_manual = get(self, 'model.erp_mgi_2_user_manual');
+  const erp_mgi_3_terms_of_use = get(self, 'model.erp_mgi_3_terms_of_use');
+  const erp_aoi_1_order_type = get(self, 'model.erp_aoi_1_order_type.id');
+
+  let missing = [];
+  let missing_str = '';
+
+  let noControls = false;
+  let message = `eosc.resource.${method}.message`;
+
+  if (!erp_bai_1_name) { missing.push('erp_bai_1_name') }
+  if (!erp_bai_2_service_organisation) { missing.push('erp_bai_2_service_organisation') }
+  if (!erp_bai_4_webpage) { missing.push('erp_bai_4_webpage') }
+  if (!erp_mri_1_description) { missing.push('erp_mri_1_description') }
+  if (!erp_mri_2_tagline) { missing.push('erp_mri_2_tagline') }
+  if (!erp_mri_3_logo) { missing.push('erp_mri_3_logo') }
+  if (!erp_cli_1_scientific_domain) { missing.push('erp_cli_1_scientific_domain') };
+  if (!erp_cli_2_scientific_subdomain) { missing.push('erp_cli_2_scientific_subdomain')};
+  if (!erp_cli_3_category) { missing.push('erp_cli_3_category')};
+  if (!erp_cli_4_subcategory) { missing.push('erp_cli_4_subcategory')};
+  if (!erp_cli_5_target_users) { missing.push('erp_cli_5_target_users')};
+  if (!erp_gla_1_geographical_availability) { missing.push('erp_gla_1_geographical_availability')};
+  if (!erp_gla_2_language) { missing.push('erp_gla_2_language')};
+  if (!main_contact) { missing.push('main_contact')};
+  if (!public_contact) { missing.push('public_contact')};
+  if (!erp_coi_13_helpdesk_email) { missing.push('erp_coi_13_helpdesk_email')};
+  if (!erp_coi_14_security_contact_email) { missing.push('erp_coi_14_security_contact_email')};
+  if (!erp_mti_1_technology_readiness_level) { missing.push('erp_mti_1_technology_readiness_level')};
+  if (!erp_mgi_2_user_manual) { missing.push('erp_mgi_2_user_manual')};
+  if (!erp_mgi_3_terms_of_use) { missing.push('erp_mgi_3_terms_of_use')};
+  if (!erp_aoi_1_order_type) { missing.push('erp_aoi_1_order_type')};
+
+  const i18n = get(self, 'i18n');
+
+  if (missing.length > 0) {
+    missing.forEach(function(el) {
+      let field = `resource.fields.${el}`;
+      let trans = i18n.t(field);
+      missing_str += `<li>${trans}</li>`;
+    })
+    noControls = true;
+    message = get(self, 'i18n').t(`eosc.resource.${method}.message_missing`, {
+      list_missing: `<ul>${missing_str}</ul>`,
+    });
+  }
+
+  return {
+    ok: `eosc.resource.${method}.ok`,
+    cancel: 'cancel',
+    title: `eosc.resource.${method}.title`,
+    message,
+    noControls,
+  }
+
+}
+
 
 const postResourceEOSC = {
   label: 'eosc.resource.post.label',
@@ -45,6 +120,7 @@ const postResourceEOSC = {
     'model.eosc-portal-id',
     'model.erp_bai_2_service_organisation',
     function(){
+
       if ( DISABLED ) { return true }
       let role = get(this, 'role');
       let user_org = get(this, 'session.session.authenticated.organisation');
@@ -90,78 +166,7 @@ const postResourceEOSC = {
     'model.erp_mgi_3_terms_of_use',
     'model.erp_aoi_1_order_type.id',
     function(){
-      let erp_bai_1_name = get(this, 'model.erp_bai_1_name');
-      let erp_bai_2_service_organisation = get(this, 'model.erp_bai_2_service_organisation.id');
-      let erp_bai_4_webpage = get(this, 'model.erp_bai_4_webpage');
-      let erp_mri_1_description = get(this, 'model.erp_mri_1_description');
-      let erp_mri_2_tagline = get(this, 'model.erp_mri_2_tagline');
-      let erp_mri_3_logo = get(this, 'model.erp_mri_3_logo');
-      let erp_cli_1_scientific_domain = get(this, 'model.erp_cli_1_scientific_domain');
-      let erp_cli_2_scientific_subdomain = get(this, 'model.erp_cli_2_scientific_subdomain');
-      let erp_cli_3_category = get(this, 'model.erp_cli_3_category');
-      let erp_cli_4_subcategory = get(this, 'model.erp_cli_4_subcategory');
-      let erp_cli_5_target_users = get(this, 'model.erp_cli_5_target_users').content.length > 0;
-      let erp_gla_1_geographical_availability = get(this, 'model.erp_gla_1_geographical_availability');
-      let erp_gla_2_language = get(this, 'model.erp_gla_2_language');
-      let main_contact = get(this, 'model.main_contact.id');
-      let public_contact = get(this, 'model.public_contact.id');
-      let erp_coi_13_helpdesk_email = get(this, 'model.erp_coi_13_helpdesk_email');
-      let erp_coi_14_security_contact_email = get(this, 'model.erp_coi_14_security_contact_email');
-      let erp_mti_1_technology_readiness_level = get(this, 'model.erp_mti_1_technology_readiness_level.id');
-      let erp_mgi_2_user_manual = get(this, 'model.erp_mgi_2_user_manual');
-      let erp_mgi_3_terms_of_use = get(this, 'model.erp_mgi_3_terms_of_use');
-      let erp_aoi_1_order_type = get(this, 'model.erp_aoi_1_order_type.id');
-
-      let missing = [];
-      let missing_str = '';
-
-      let noControls = false;
-      let message = 'eosc.resource.post.message';
-
-      if (!erp_bai_1_name) { missing.push('erp_bai_1_name') }
-      if (!erp_bai_2_service_organisation) { missing.push('erp_bai_2_service_organisation') }
-      if (!erp_bai_4_webpage) { missing.push('erp_bai_4_webpage') }
-      if (!erp_mri_1_description) { missing.push('erp_mri_1_description') }
-      if (!erp_mri_2_tagline) { missing.push('erp_mri_2_tagline') }
-      if (!erp_mri_3_logo) { missing.push('erp_mri_3_logo') }
-      if (!erp_cli_1_scientific_domain) { missing.push('erp_cli_1_scientific_domain') };
-      if (!erp_cli_2_scientific_subdomain) { missing.push('erp_cli_2_scientific_subdomain')};
-      if (!erp_cli_3_category) { missing.push('erp_cli_3_category')};
-      if (!erp_cli_4_subcategory) { missing.push('erp_cli_4_subcategory')};
-      if (!erp_cli_5_target_users) { missing.push('erp_cli_5_target_users')};
-      if (!erp_gla_1_geographical_availability) { missing.push('erp_gla_1_geographical_availability')};
-      if (!erp_gla_2_language) { missing.push('erp_gla_2_language')};
-      if (!main_contact) { missing.push('main_contact')};
-      if (!public_contact) { missing.push('public_contact')};
-      if (!erp_coi_13_helpdesk_email) { missing.push('erp_coi_13_helpdesk_email')};
-      if (!erp_coi_14_security_contact_email) { missing.push('erp_coi_14_security_contact_email')};
-      if (!erp_mti_1_technology_readiness_level) { missing.push('erp_mti_1_technology_readiness_level')};
-      if (!erp_mgi_2_user_manual) { missing.push('erp_mgi_2_user_manual')};
-      if (!erp_mgi_3_terms_of_use) { missing.push('erp_mgi_3_terms_of_use')};
-      if (!erp_aoi_1_order_type) { missing.push('erp_aoi_1_order_type')};
-      console.log(missing);
-
-      const i18n = get(this, 'i18n');
-
-      if (missing.length > 0) {
-        missing.forEach(function(el) {
-          let field = `resource.fields.${el}`;
-          let trans = i18n.t(field);
-          missing_str += `<li>${trans}</li>`;
-        })
-        noControls = true;
-        message = get(this, 'i18n').t('eosc.resource.post.message_missing', {
-          list_missing: `<ul>${missing_str}</ul>`,
-        });
-      }
-
-      return {
-        ok: 'eosc.resource.post.ok',
-        cancel: 'cancel',
-        title: 'eosc.resource.post.title',
-        message,
-        noControls,
-      }
+      return prompt(this, 'post');
     }
   ),
 };
@@ -246,78 +251,7 @@ const putResourceEOSC = {
     'model.erp_mgi_3_terms_of_use',
     'model.erp_aoi_1_order_type.id',
     function(){
-      let erp_bai_1_name = get(this, 'model.erp_bai_1_name');
-      let erp_bai_2_service_organisation = get(this, 'model.erp_bai_2_service_organisation.id');
-      let erp_bai_4_webpage = get(this, 'model.erp_bai_4_webpage');
-      let erp_mri_1_description = get(this, 'model.erp_mri_1_description');
-      let erp_mri_2_tagline = get(this, 'model.erp_mri_2_tagline');
-      let erp_mri_3_logo = get(this, 'model.erp_mri_3_logo');
-      let erp_cli_1_scientific_domain = get(this, 'model.erp_cli_1_scientific_domain');
-      let erp_cli_2_scientific_subdomain = get(this, 'model.erp_cli_2_scientific_subdomain');
-      let erp_cli_3_category = get(this, 'model.erp_cli_3_category');
-      let erp_cli_4_subcategory = get(this, 'model.erp_cli_4_subcategory');
-      let erp_cli_5_target_users = get(this, 'model.erp_cli_5_target_users').content.length > 0;
-      let erp_gla_1_geographical_availability = get(this, 'model.erp_gla_1_geographical_availability');
-      let erp_gla_2_language = get(this, 'model.erp_gla_2_language');
-      let main_contact = get(this, 'model.main_contact.id');
-      let public_contact = get(this, 'model.public_contact.id');
-      let erp_coi_13_helpdesk_email = get(this, 'model.erp_coi_13_helpdesk_email');
-      let erp_coi_14_security_contact_email = get(this, 'model.erp_coi_14_security_contact_email');
-      let erp_mti_1_technology_readiness_level = get(this, 'model.erp_mti_1_technology_readiness_level.id');
-      let erp_mgi_2_user_manual = get(this, 'model.erp_mgi_2_user_manual');
-      let erp_mgi_3_terms_of_use = get(this, 'model.erp_mgi_3_terms_of_use');
-      let erp_aoi_1_order_type = get(this, 'model.erp_aoi_1_order_type.id');
-
-      let missing = [];
-      let missing_str = '';
-
-      let noControls = false;
-      let message = 'eosc.resource.put.message';
-
-      if (!erp_bai_1_name) { missing.push('erp_bai_1_name') }
-      if (!erp_bai_2_service_organisation) { missing.push('erp_bai_2_service_organisation') }
-      if (!erp_bai_4_webpage) { missing.push('erp_bai_4_webpage') }
-      if (!erp_mri_1_description) { missing.push('erp_mri_1_description') }
-      if (!erp_mri_2_tagline) { missing.push('erp_mri_2_tagline') }
-      if (!erp_mri_3_logo) { missing.push('erp_mri_3_logo') }
-      if (!erp_cli_1_scientific_domain) { missing.push('erp_cli_1_scientific_domain') };
-      if (!erp_cli_2_scientific_subdomain) { missing.push('erp_cli_2_scientific_subdomain')};
-      if (!erp_cli_3_category) { missing.push('erp_cli_3_category')};
-      if (!erp_cli_4_subcategory) { missing.push('erp_cli_4_subcategory')};
-      if (!erp_cli_5_target_users) { missing.push('erp_cli_5_target_users')};
-      if (!erp_gla_1_geographical_availability) { missing.push('erp_gla_1_geographical_availability')};
-      if (!erp_gla_2_language) { missing.push('erp_gla_2_language')};
-      if (!main_contact) { missing.push('main_contact')};
-      if (!public_contact) { missing.push('public_contact')};
-      if (!erp_coi_13_helpdesk_email) { missing.push('erp_coi_13_helpdesk_email')};
-      if (!erp_coi_14_security_contact_email) { missing.push('erp_coi_14_security_contact_email')};
-      if (!erp_mti_1_technology_readiness_level) { missing.push('erp_mti_1_technology_readiness_level')};
-      if (!erp_mgi_2_user_manual) { missing.push('erp_mgi_2_user_manual')};
-      if (!erp_mgi_3_terms_of_use) { missing.push('erp_mgi_3_terms_of_use')};
-      if (!erp_aoi_1_order_type) { missing.push('erp_aoi_1_order_type')};
-      console.log(missing);
-
-      const i18n = get(this, 'i18n');
-
-      if (missing.length > 0) {
-        missing.forEach(function(el) {
-          let field = `resource.fields.${el}`;
-          let trans = i18n.t(field);
-          missing_str += `<li>${trans}</li>`;
-        })
-        noControls = true;
-        message = get(this, 'i18n').t('eosc.resource.put.message_missing', {
-          list_missing: `<ul>${missing_str}</ul>`,
-        });
-      }
-
-      return {
-        ok: 'eosc.resource.put.ok',
-        cancel: 'cancel',
-        title: 'eosc.resource.put.title',
-        message,
-        noControls,
-      }
+      return prompt(this, 'put');
     }
   ),
 };
