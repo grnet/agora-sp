a5168ed2a553f3e9039614e4bdb4bfd56cd49a49
Kostas Koumantaros <kkoum@admin.grnet.gr>
2021-12-14 10:05:49 +0000

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     Features: ['authenticate', 'eosc_integration' ],
-    'Agora User Documentation' : ['profile', 'resources', 'providers', 'contact', 'resource_admins', 'users', 'settings']
+    'Agora User Documentation' : ['profile', 'resources', 'providers', 'contact', 'resource_admins', 'users', 'settings'],
+    'Policies' : ['terms','privacy-statement','cookie-policy']
   },
 };
