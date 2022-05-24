06329f7d9eb2d4d84fe3b254ed241ec883ff84a6
Kostas Evangelou <kevangel@admin.grnet.gr>
2022-05-24 13:09:48 +0300

    Fix null objects

--- ui/app/components/multiple-pairs-display.js
+++ ui/app/components/multiple-pairs-display.js
@@ -4,6 +4,18 @@ const {
     computed,
 } = Ember;
 
+function isValidHttpUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
 export default Ember.Component.extend({
   valueObj: computed('value', function(){
     let val = get(this, 'value');
@@ -11,12 +23,21 @@ export default Ember.Component.extend({
     try {
       res = JSON.parse(val);
     } catch(e) {
+      if (typeof val === 'string' && isValidHttpUrl(val)) {
+        res['multimedia link'] = val
+      }
       console.log('Value needs to be in JSON format')
     }
     return res;
   }),
   valueArr: computed('valueObj', function(){
     let valueObj = get(this, 'valueObj');
-    return Object.entries(valueObj);
+    console.log(valueObj)
+    if (!!valueObj) {
+      return Object.entries(valueObj);
+    }
+    else {
+      return [];
+    }
   }),
 });
