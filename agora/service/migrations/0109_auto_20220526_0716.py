f10c76b653260e5f16040ee25daed2dad030215c
Kostas Evangelou <kevangel@admin.grnet.gr>
2022-05-26 12:35:59 +0300

    Add new component to resource multimedia

--- agora/service/migrations/0109_auto_20220526_0716.py
+++ agora/service/migrations/0109_auto_20220526_0716.py
@@ -3,18 +3,31 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-
+from service.models import Resource
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('service', '0108_auto_20220526_0715'),
     ]
+    
+    def migrate_string_to_json(apps, schema):
+        for sample in Resource.objects.all():
+            try:
+                if sample.erp_mri_4_multimedia!=None:
+                    sample.erp_mri_4_multimedia = '{"multimedia link": "' + sample.erp_mri_4_multimedia + '"}'
+                    sample.save()
+            except:
+                print('Cannot convert {} object'.format(sample.pk))
+
 
     operations = [
         migrations.AlterField(
             model_name='resource',
             name='erp_mri_4_multimedia',
             field=models.TextField(blank=True, default=None, null=True),
+        ),
+        migrations.RunPython(
+            migrate_string_to_json
         ),
     ]
