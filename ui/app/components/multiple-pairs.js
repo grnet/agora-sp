4f2ab7c5a08de132ac8e5f65a5129a4d1a66338e
Olga Brani <olgabrani@grnet.gr>
2022-05-26 18:39:54 +0300

    ui: Make multiple pairs component text configurable

--- ui/app/components/multiple-pairs.js
+++ ui/app/components/multiple-pairs.js
@@ -2,19 +2,46 @@ import Ember from 'ember';
 const {
     get, set,
     computed,
-    computed: { or, alias },
+    computed: { or, alias, reads },
     on
 } = Ember;
 
 export default Ember.Component.extend({
+  i18n: Ember.inject.service(),
   classNames: ['multiple-pairs'],
   tagName: 'md-input-container',
   error: false,
 
+  actionTitle: reads('fattrs.actionTitle'),
+
+  item1: reads('fattrs.item1'),
+  item2: reads('fattrs.item2'),
+
+  input1: computed('', function(){
+    let item1 = get(this, 'item1');
+    return Ember.$(`#${item1}`);
+  }),
+
+  input2: computed('', function(){
+    let item2 = get(this, 'item2');
+    return Ember.$(`#${item2}`);
+  }),
+  errorHint: computed('', function(){
+    let i18n = get(this, 'i18n');
+    let item1 = get(this, 'item1');
+    let trans1 = i18n.t(item1);
+    let item2 = get(this, 'item2');
+    let trans2 = i18n.t(item2);
+    return `Both ${trans1} and ${trans2} must be filled`;
+  }),
+
   clearInput(){
-    Ember.$('#multimedia-name').val(null);
-    Ember.$('#multimedia-url').val(null);
+    let input1 = get(this, 'input1');
+    let input2 = get(this, 'input2');
+    input1.val(null);
+    input2.val(null);
   },
+
   valueObj: computed('value', function(){
     let val = get(this, 'value');
     let res = {};
@@ -25,6 +52,7 @@ export default Ember.Component.extend({
     }
     return res;
   }),
+
   valueArr: computed('valueObj', function(){
     let valueObj = get(this, 'valueObj');
     if (!!valueObj) {
@@ -37,24 +65,26 @@ export default Ember.Component.extend({
 
 
   actions: {
+
     removePair(name, url){
       let valueObj = get(this, 'valueObj');
       if (valueObj[name] == url) {
         delete valueObj[name];
       }
       return this.sendAction('onChange', JSON.stringify(valueObj));
-
-
     },
+
     addPair(e) {
-      let name = Ember.$('#multimedia-name').val();
-      let url = Ember.$('#multimedia-url').val();
+      let input1 = get(this, 'input1');
+      let input1_val = input1.val();
+      let input2 = get(this, 'input2');
+      let input2_val = input2.val();
       let valueObj = get(this, 'valueObj');
       if (!valueObj) {
         valueObj = {};
       }
-      valueObj[name] = url;
-      if (!name || !url) {
+      valueObj[input1_val] = input2_val;
+      if (!input1_val || !input2_val) {
         set(this, 'error', true);
         return;
       } else {
